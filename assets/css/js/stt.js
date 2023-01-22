b52748a9a8a7e538996c2a473edbae603a14335e
Rudder <ben@ruddernation.com>
2023-01-22 15:54:14 +0000

    Delete assets/css directory

--- assets/css/js/stt.js
+++
@@ -1,9 +0,0 @@
-var topPage = document.getElementById(`top-page`)
-
-topPage.onclick = () => window.scrollTo({ top: 0, behavior: 'smooth'}) 
-
-window.onscroll = () => {
-  window.scrollY > 500 
-    ? (topPage.style.display = `block`)
-    : (topPage.style.display = `none`)
-}
